090df1b8a141dfba3c5bfd915e018cffad802226
eberzina <berzina3633@gmail.com>
2026-05-22 13:47:50 +0300

    Add style to carousel images

--- components/ProductImageCarousel.tsx
+++ components/ProductImageCarousel.tsx
@@ -32,13 +32,34 @@ export function ProductImageCarousel() {
     <div className="w-screen -ml-8 mb-8 relative">
       <button 
         onClick={() => handleCarouselNavigation("previous")}
-        className="flex justify-center items-center bg-[hsl(var(--white))] w-12 h-12 rounded-full absolute top-1/2 -translate-y-1/2 left-4 z-10 cursor-pointer">
+        className="flex justify-center items-center bg-[hsl(var(--white))] w-12 h-12 rounded-full absolute top-1/2 -translate-y-1/2 left-4 z-10 cursor-pointer md:hidden">
         <Image className="w-3 h-4.5" src={IconPrevious} alt="Previous image" />
       </button>
-      <Image src={images[selectedImageIndex].src} alt={`Product image ${selectedImageIndex + 1}`} className="h-100 object-none" /> 
+      <Image src={images[selectedImageIndex].src} alt={`Product image ${selectedImageIndex + 1}`} className="h-100 object-none" />
+      <div className="hidden md:flex md:flex-row md:justify-start md:items-center md:gap-6 mt-4">
+        <button
+          className={`rounded-lg cursor-pointer ${selectedImageIndex === 0 ? "ring-2 ring-[hsl(var(--orange))] relative overflow-hidden" : ""}`}
+          onClick={() => setSelectedImageIndex(0)}
+        >
+          <Image src={ProductImage1} alt="Thumbnail 1" className="w-16 h-16 object-cover rounded-lg"  />
+          <div className="absolute w-16 h-16 inset-0 bg-[hsl(var(--light-grayish-blue)/0.7)] z-100"></div>
+        </button>
+        <button className={`rounded-lg cursor-pointer ${selectedImageIndex === 1 ? "ring-2 ring-[hsl(var(--orange))] relative overflow-hidden" : ""}`} onClick={() => setSelectedImageIndex(1)}>
+          <Image src={ProductImage2} alt="Thumbnail 2" className="w-16 h-16 object-cover rounded-lg" />
+          <div className="absolute w-16 h-16 inset-0 bg-[hsl(var(--light-grayish-blue)/0.7)] z-100"></div>
+        </button>
+        <button className={`rounded-lg cursor-pointer ${selectedImageIndex === 2 ? "ring-2 ring-[hsl(var(--orange))] relative overflow-hidden" : ""}`} onClick={() => setSelectedImageIndex(2)}>
+          <Image src={ProductImage3} alt="Thumbnail 3" className="w-16 h-16 object-cover rounded-lg" />
+          <div className="absolute w-16 h-16 inset-0 bg-[hsl(var(--light-grayish-blue)/0.7)] z-100"></div>
+        </button>
+        <button className={`rounded-lg cursor-pointer ${selectedImageIndex === 3 ? "ring-2 ring-[hsl(var(--orange))] relative overflow-hidden" : ""}`} onClick={() => setSelectedImageIndex(3)}>
+          <Image src={ProductImage4} alt="Thumbnail 4" className="w-16 h-16 object-cover rounded-lg" />
+          <div className="absolute w-16 h-16 inset-0 bg-[hsl(var(--light-grayish-blue)/0.7)] z-100"></div>
+        </button>
+      </div>
       <button 
         onClick={() => handleCarouselNavigation("next")}
-        className="flex justify-center items-center bg-[hsl(var(--white))] w-12 h-12 rounded-full absolute top-1/2 -translate-y-1/2 right-8 z-10 cursor-pointer">
+        className="flex justify-center items-center bg-[hsl(var(--white))] w-12 h-12 rounded-full absolute top-1/2 -translate-y-1/2 right-8 z-10 cursor-pointer md:hidden">
         <Image className="w-3 h-4.5" src={IconNext} alt="Next image" />
       </button>
     </div>
