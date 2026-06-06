9540e459525ecd4b9bd54de293ff42591b9f6265
eberzina <berzina3633@gmail.com>
2026-06-06 13:52:31 +0300

    Style Cart and add checkout button

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -20,23 +20,30 @@ export function Cart({cartQuantity, setCartQuantity}: AddToCartProps) {
   }
 
   return (
-    <div className="flex flex-col items-stretch absolute top-30 left-4 right-4 h-90 rounded-xl bg-[hsl(var(--white))] z-1000 lg: lg:h-60 lg:w-80 lg:right-12 lg:left-auto lg:shadow-lg/20">
+    <div className="flex flex-col absolute top-30 left-4 right-4 rounded-xl bg-[hsl(var(--white))] z-39 lg:right-12 lg:left-auto lg:shadow-lg/20">
       <h1 className="text-[hsl(var(--black))] font-bold py-6 px-4 border-b border-[hsl(var(--grayish-blue))]">Cart</h1>
       {cartQuantity === 0
         ? (
-          <p className="text-[hsl(var(--dark-grayish-blue))] flex flex-col justify-center items-center h-full">Your cart is empty.</p>
+          <div className="flex flex-row justify-center items-center p-4 min-h-40 min-w-[348.75px]">
+            <p className="text-[hsl(var(--dark-grayish-blue))] flex flex-col justify-center items-center h-full">Your cart is empty.</p>
+          </div>
         )
         : (
-          <div className="flex flex-row justify-center items-center gap-4 h-full">
-            <div className="rounded-lg overflow-hidden">
-              <Image src={product.picture} alt="Empty cart" loading="eager" className="w-14 h-14" />
+          <div className="flex flex-col gap-4 p-4">
+            <div className="flex flex-row justify-center items-center gap-4">
+              <div className="rounded-lg overflow-hidden">
+                <Image src={product.picture} alt="Empty cart" loading="eager" className="w-14 h-14" />
+              </div>
+              <div className="flex flex-col justify-center items-start gap-1">
+                <p>{product.name}</p>
+                <p>${product.price.toFixed(2)}  x {cartQuantity} <span className="font-bold text-[hsl(var(--black))]">${(product.price * cartQuantity).toFixed(2)}</span></p>
+              </div>
+              <button type="button" onClick={deleteCartItem}>
+                <Image src={IconDelete} alt="Delete icon" loading="eager" className="w-4 cursor-pointer" />
+              </button>
             </div>
-            <div className="flex flex-col justify-center items-start gap-1">
-              <p>{product.name}</p>
-              <p>${product.price.toFixed(2)}  x {cartQuantity} <span className="font-bold text-[hsl(var(--black))]">${(product.price * cartQuantity).toFixed(2)}</span></p>
-            </div>
-            <button onClick={deleteCartItem}>
-              <Image src={IconDelete} alt="Delete icon" loading="eager" className="w-4 cursor-pointer" />
+            <button type="button" className="bg-[hsl(var(--orange))] text-[hsl(var(--black))] font-bold py-4 rounded-lg cursor-pointer lg:flex-2 hover:bg-[hsl(var(--orange)/0.6)] transition-colors duration-200">
+              Checkout
             </button>
           </div>
         )}
--- components/Header.tsx
+++ components/Header.tsx
@@ -78,21 +78,18 @@ export function Header({cartQuantity, setCartQuantity, showCart, setShowCart}: A
         </ul>
       </div>
       <div className="flex flex-row items-center justify-center gap-8 h-full">
-        <div className="relative">
+        <button className="relative group cursor-pointer" onClick={showCartNow} type="button">
           {cartQuantity > 0 && (
-            <div className="absolute -top-2 -right-2 bg-[hsl(var(--orange))] text-[hsl(var(--white))] w-5 h-4 rounded-lg flex items-center justify-center text-xs font-bold z-50">
+            <div className="absolute -top-2 -right-2 bg-[hsl(var(--orange))] text-[hsl(var(--white))] w-5 h-4 rounded-lg flex items-center justify-center text-xs font-bold z-39">
               {cartQuantity}
             </div>
           )}
-          <button 
-            onClick={showCartNow}
-            type="button"
-            className="group cursor-pointer">
+          <div>
             <svg width="22" height="20">
               <path d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z" className="fill-[hsl(var(--dark-grayish-blue))] group-hover:fill-[hsl(var(--black))] transition-all duration-200" />
             </svg>
-          </button>
-        </div>
+          </div>
+        </button>
         <button
           type="button"
           className="rounded-full overflow-hidden border-2 border-transparent hover:border-[hsl(var(--orange))] transition-all duration-200 cursor-pointer">
